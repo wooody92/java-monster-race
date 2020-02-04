fa6a292333914e323dff75f6f0621a9bd20984c2
wooody92 <pkower4@naver.com>
2020-02-04 13:20:11 +0900

    Make logic method
    - 단순 기능 요구사항 구현 및 실행 테스트
    - display(): 사용자로부터 monterCnt, trial 입력받아, calMove() loop 생성
    - calMove(): random 값이 4이상일 경우에만 "-" 출력

--- src/main/java/step1/Main.java
+++ src/main/java/step1/Main.java
@@ -1,7 +1,9 @@
 package step1;
 
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         MonsterRace monsterRace = new MonsterRace();
         monsterRace.display();
     }
--- src/main/java/step1/MonsterRace.java
+++ src/main/java/step1/MonsterRace.java
@@ -1,11 +1,39 @@
 package step1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Random;
+
 public class MonsterRace {
-    public void display() {
-        // 사용자 입력 및 결과 출력 메서드
+    public static void display() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        System.out.println("< 스릴만점 건전한 몬스터 경주 >");
+        System.out.println("몬스터는 모두 몇 마리 인가요? ");
+        int monsterCnt = Integer.parseInt(br.readLine());
+
+        System.out.println("시도할 회수는 몇 회 인가요? ");
+        int trial = Integer.parseInt(br.readLine());
+
+        System.out.println("< 실행 결과 >");
+
+        for(int i=0; i<monsterCnt; i++) {
+            for (int j = 0; j < trial; j++) {
+                calMove();
+            }
+            System.out.println();
+        }
+        br.close();
     }
 
-    public void calMove() {
-        // 움직임 결과 연산 메서드
+    public static void calMove() {
+        Random rd = new Random();
+        int random = rd.nextInt(10);
+
+        if (!(random >= 4)) return;
+        for (int i = 0; i < random; i++) {
+            System.out.print("-");
+        }
     }
 }
