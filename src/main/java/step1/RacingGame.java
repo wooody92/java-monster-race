e4a64e500282b3925b2fa808d6a747b18046a90c
woody92 <pkower4@naver.com>
2020-02-05 14:54:31 +0900

    refactor for reducing indent depth
    indent depth를 1단계로 줄이기 위해 아래와 같이 메서드를 분리
    1. racing()
     - monster string array 크기를 몬스터 숫자만큼 선언
     - 각각의 몬스터에 이동 결과값 입력
     - 출력
    2. getMoveResult()
     - 시도횟수만큼 "-" 더하여 결과값 반환

--- src/main/java/step1/RacingGame.java
+++ src/main/java/step1/RacingGame.java
@@ -4,26 +4,37 @@ import java.util.Random;
 
 public class RacingGame {
     int monsterNumber;
+
     public RacingGame(int monsterNumber) {
         this.monsterNumber = monsterNumber;
     }
 
     public void racing(int trialNumber) {
         System.out.println("< 실행 결과 >");
-        for(int i = 0; i < monsterNumber; i++) {
-            for (int j = 0; j < getMoveCnt(trialNumber); j++) {
-                System.out.print("-");
-            }
-            System.out.println();
+        String[] monster = new String[monsterNumber];
+
+        for (String moveResult : monster) {
+            moveResult = getMoveResult(trialNumber);
+            System.out.println(moveResult);
         }
+    }
+
+    public String getMoveResult(int trialNumber) {
+        String moveResult = "";
+        for (int j = 0; j < getMoveCnt(trialNumber); j++) {
+            moveResult += "-";
+        }
+
+        return moveResult;
     }
 
     public int getMoveCnt(int trialNumber) {
         int moveCnt = 0;
 
-        for(int i = 0; i < trialNumber; i++){
+        for (int i = 0; i < trialNumber; i++) {
             moveCnt += checkMove();
         }
+
         return moveCnt;
     }
 
@@ -32,7 +43,7 @@ public class RacingGame {
         int random = rd.nextInt(10);
         int move = 0;
 
-        if(!(random >= 4)) return 0;
+        if (!(random >= 4)) return 0;
         move++;
 
         return move;
