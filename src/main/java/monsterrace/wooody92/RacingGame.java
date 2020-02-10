5aecb8deb63c64e9922253b148451388424c333a
wooody92 <pkower4@naver.com>
2020-02-10 19:31:43 +0900

    add prototype method
    중복코드 제거, 복잡도 등 생각하지않고 우선 기능만 구현 해보았습니다. 인터페이스 예외처리 등을 이용하여 코드 수정 계획입니다.

--- src/main/java/monsterrace/wooody92/RacingGame.java
+++ src/main/java/monsterrace/wooody92/RacingGame.java
@@ -16,36 +16,71 @@ public class RacingGame {
 
     public void racing() {
         for (int i = 0; i < monster.length; i++) {
-            this.result.add(monster[i].name + "[" + monster[i].type + "]: " +getMoveResult());
+            this.result.add(monster[i].name + "[" + monster[i].type + "]: " +getMoveResult(monster[i].type));
         }
     }
 
-    public String getMoveResult() {
+    public String getMoveResult(String monsterType) {
         String moveResult = "";
-        for (int j = 0; j < getMoveCnt(); j++) {
+        int move = getMoveCnt(monsterType);
+
+        for (int i = 0; i < move; i++) {
             moveResult += "-";
         }
         return moveResult;
     }
 
-    public int getMoveCnt() {
+    public int getMoveCnt(String monsterType) {
         int moveCnt = 0;
 
         for (int i = 0; i < trialNumber; i++) {
-            moveCnt += checkMove();
+            switch (monsterType) {
+                case "0" : moveCnt += checkMove0(); break;
+                case "1" : moveCnt += checkMove1(); break;
+                case "2" : moveCnt += checkMove2(); break;
+            }
+            System.out.println("monstertype + moveCnt: " + monsterType + moveCnt);
         }
         return moveCnt;
     }
 
-    public int checkMove() {
-        Random rd = new Random();
-        int random = rd.nextInt(10);
+    public int checkMove0() {
         final int MINIMUM_NUM = 4;
         int move = 0;
 
-        if (!(random >= MINIMUM_NUM)) return 0;
+        if (!(getRandom() >= MINIMUM_NUM)) return 0;
         move++;
 
         return move;
+    }
+
+    public int checkMove1() {
+        final int MINIMUM_NUM = 6;
+        int move = 0;
+
+        if (!(getRandom() >= MINIMUM_NUM)) return 0;
+        move+=3;
+
+        return move;
+    }
+
+    public int checkMove2() {
+        final int MINIMUM_NUM = 9;
+        int move = 0;
+
+        if (!(getRandom() >= MINIMUM_NUM)) return 0;
+        move+=getRandom2();
+
+        return move;
+    }
+
+    public int getRandom() {
+        Random rd = new Random();
+        return rd.nextInt(10);
+    }
+
+    public int getRandom2() {
+        Random rd = new Random();
+        return rd.nextInt(100);
     }
 }
