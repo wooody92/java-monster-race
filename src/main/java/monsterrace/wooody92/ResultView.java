5c464d0ac8cb1344898a273d4233a1ded3369c0a
wooody92 <pkower4@naver.com>
2020-02-12 20:53:32 +0900

    refactor printResult method using stream
    결과값을 스트림을 이용해 출력 해보았습니다.

--- src/main/java/monsterrace/wooody92/ResultView.java
+++ src/main/java/monsterrace/wooody92/ResultView.java
@@ -1,11 +1,14 @@
 package monsterrace.wooody92;
 
+import java.util.stream.Stream;
+
 public class ResultView {
     public static void printResult(RacingGame racingGame) {
         System.out.println("\n< 실행 결과 >");
-        for (String resultMove : racingGame.result) {
-            System.out.println(resultMove);
-        }
+
+        Stream<String> stringStream = racingGame.result.stream();
+        stringStream.forEach(System.out::println);
+
         System.out.println("\n축하합니다! 우승자는 바로: " + racingGame.getWinner());
     }
 }
